278fd3f97924fbcffc589ccec5ca2a976cd8f6ee
Leo Kov <leo@Leos-MacBook-Pro.local>
2023-04-25 23:35:42 +0200

    Admiral

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import './App.css';
 *  http://www.webtoolkit.info
 *
 **/
+const liveInd = ' --- LIVE ---';
+
 var Base64 = {
 
   // private property
@@ -168,7 +170,7 @@ var Base64 = {
 //const updateAfter = 700;
 //const searchStateToUrl = (searchState) =>
 //  searchState ? `${window.location.pathname}?${qs.stringify(searchState)}` : '';
-const hasDuplicates = (arr) => arr.length !== new Set(arr).size;
+//const hasDuplicates = (arr) => arr.length !== new Set(arr).size;
 
 class App extends Component {
   constructor() {
@@ -181,7 +183,8 @@ class App extends Component {
       resultsSbbet: {},
       resultsMeridian: {},
       resultsVolcano: {},
-      resultsMaxbet: {}
+      resultsMaxbet: {},
+      resultsAdmiral: {},
     };
 
     window.addEventListener('popstate', ({ state: searchState }) => {
@@ -191,10 +194,11 @@ class App extends Component {
 
   onSearchStateChange = (searchState) => {
 
-    //let url = `https://www.lobbet.me/ibet/async/live/multy/-1.json`;
+    // -----
+    // LOBBET
+    // -----
     let url = `https://www.lobbet.me/ibet/async/live/multy/1.json`;
-    request({url, method: 'POST'}, (error, response, body) => {
-        // Do more stuff with 'body' here
+     request({url, method: 'POST'}, (error, response, body) => {
         if (body) {
           const parsedBody = JSON.parse(body);
           //console.log('LOBBBB1: ', parsedBody);
@@ -237,10 +241,91 @@ class App extends Component {
         }
     });
 
-    url = `https://solitary-wind-aed0.lenkovlen9913.workers.dev/?https://www.premierbet.me/balance9876/user/logged`;
-//console.log('url: ', url);
-    const fetchOpts = {
+    // -----
+    // ADMIRAL
+    // -----
 
+    fetch(`https://quiet-sky-9919.lenkovlen9913.workers.dev/?https://webapi.admiralbet.me/SportBookCacheWeb/api/offer/SearchEventsWeb/${searchState.query}/false`, {
+      "headers": {
+        //"accept": "application/json, text/plain, */*",
+        //"accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+        //"content-type": "application/json",
+        //"language": "sr-Latn",
+        "officeid": "1175",
+        //"sec-ch-ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
+        //"sec-ch-ua-mobile": "?0",
+        //"sec-ch-ua-platform": "\"macOS\"",
+        //"sec-fetch-dest": "empty",
+        //"sec-fetch-mode": "cors",
+        //"sec-fetch-site": "same-site",
+        //"Referer": "https://website.admiralbet.me/",
+        //"Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      "body": null,
+      "method": "GET"
+    })
+    .then(response => {
+      //console.log('ADMIRAL : ', response);
+      return response.json();
+    })
+    .then((response) => {
+      console.log('ADMiRAL PREMATCH SEARCH: ', response);
+      const neededMatches = response.map((m) => m.name);
+      console.log('neededMatches: ', neededMatches);
+      // Fetch live matches, Admiral API has the search only for prematches
+      
+      fetch("https://quiet-sky-9919.lenkovlen9913.workers.dev/?https://webapi.admiralbet.me/SportBookCacheWeb/api/offer/livetree/4/null/true/true/false", {
+        "headers": {
+          //"accept": "application/json, text/plain, */*",
+          //"accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+          //"content-type": "application/json",
+          //"language": "sr-Latn",
+          "officeid": "1175",
+          //"sec-ch-ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
+          //"sec-ch-ua-mobile": "?0",
+          //"sec-ch-ua-platform": "\"macOS\"",
+          //"sec-fetch-dest": "empty",
+          //"sec-fetch-mode": "cors",
+          //"sec-fetch-site": "same-site",
+          //"Referer": "https://website.admiralbet.me/",
+          //"Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+      })
+      .then(response => {
+        //console.log('ADMIRAL : ', response);
+        return response.json();
+      })
+      .then((response) => {
+        console.log('ADMiRAL LIVE: ', response);
+        response.forEach((sportType) => {
+          sportType.regions.forEach((region) => {
+            region.competitions.forEach((competition) => {
+              competition.events.forEach((event) => {
+                if (new RegExp(searchState.query, 'i').test(event.name)) {
+                  neededMatches.unshift(event.name + liveInd);
+                }
+              });
+            });
+          });
+        });
+        this.setState({ resultsAdmiral: neededMatches });
+      })
+      .catch((e) => {
+        console.log("ERROR ::: ", e);
+      });
+    })
+    .catch((e) => {
+      console.log("ERROR ::: ", e);
+    });
+
+    // -----
+    // PREMIER
+    // -----
+    
+    url = `https://solitary-wind-aed0.lenkovlen9913.workers.dev/?https://www.premierbet.me/balance9876/user/logged`;
+    const fetchOpts = {
       "body": "{}"
       //"mode": "cors",
       //"credentials": "include"
@@ -308,6 +393,10 @@ class App extends Component {
       console.log("ERROR ::: ", e);
     });
 
+    // -----
+    // SBBET
+    // -----
+
     fetch("https://n-go-grpc.sbbet.me/odds_stream.OddsStreamService/WebEventsStreamOrdered", {
       "headers": {
         "accept": "application/grpc-web-text",
@@ -349,7 +438,10 @@ class App extends Component {
 
           this.setState({ resultsSbbet: resultMatches });
         });
-
+    
+    // -----
+    // MERIDIAN
+    // -----
 
     url = `https://meridianbet.me/sails/search/page?query=${searchState.query}&locale=en`;
     request({url}, (error, response, body) => {
@@ -362,6 +454,10 @@ class App extends Component {
         }
     });
 
+    // -----
+    // VOLCANO
+    // -----
+
     url = `https://sportdataprovider.volcanobet.me/api/public/prematch/search?searchString=${searchState.query}`;
     request({url}, (error, response, body) => {
         if (body) {
@@ -371,6 +467,10 @@ class App extends Component {
           }
         }
     });
+
+    // -----
+    // MAXBET
+    // -----
 
     url = `https://api.allorigins.win/get?url=https://api.maxbet.me/search?query=${searchState.query}`;
     
@@ -383,7 +483,6 @@ class App extends Component {
       }
     })
     .catch((error) => console.log('ERROR: ', error));
-    
 
     this.setState((previousState) => {
       const hasQueryChanged =
@@ -405,13 +504,14 @@ class App extends Component {
       margin: '5px',
       padding: '10px'
     };
-    const { searchState = {}, resultsLobbet = {}, resultsPremier = {}, resultsSbbet = {}, resultsMeridian = {}, resultsVolcano = {}, resultsMaxbet = {} } = this.state;
+    const { searchState = {}, resultsLobbet = {}, resultsPremier = {}, resultsSbbet = {}, resultsMeridian = {}, resultsVolcano = {}, resultsMaxbet = {}, resultsAdmiral = {} } = this.state;
     let resultsLobbetHTML = [];
     let resultsPremierHTML = [];
     let resultsSbbetHTML = [];
     let resultsMeridianHTML = [];
     let resultsVolcanoHTML = [];
     let resultsMaxbetHTML = [];
+    let resultsAdmiralHTML = [];
     let i = 0;
     if (Object.keys(resultsLobbet).length !== 0) {
       resultsLobbet.map(element => {
@@ -449,6 +549,12 @@ class App extends Component {
         resultsMaxbetHTML.push(<p key={i}>{element.competitors.map((p) => p.name).join(' - ')} {element.live ? '--- LIVE ---':''}</p>);
       });
     }
+    if (Object.keys(resultsAdmiral).length !== 0) {
+      resultsAdmiral.map(element => {
+        i++;
+        resultsAdmiralHTML.push(<p key={i}>{element.toString()}</p>);
+      });
+    }
     return (
       <div className="App">
         <header>
@@ -480,6 +586,10 @@ class App extends Component {
             <div style={boxStyle}>
               <p>Lobbet</p>
               {resultsLobbetHTML}
+            </div>
+            <div style={boxStyle}>
+              <p>Admiral</p>
+              {resultsAdmiralHTML}
             </div>
             <div style={boxStyle}>
               <p>Premier</p>
